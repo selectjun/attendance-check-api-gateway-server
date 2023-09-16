ea79461430791590e22612db24a0fc65757adcc1
selectjundev <selectjun.dev@gmail.com>
2023-09-16 23:29:49 +0900

    [ feat ] 프로젝트 초기화

---
+++ src/test/java/com/attendancecheck/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendancecheck.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
