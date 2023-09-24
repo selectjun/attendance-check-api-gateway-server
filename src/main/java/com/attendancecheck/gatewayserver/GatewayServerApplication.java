09487d7e30d10944f698bf14e1cd55ae538d1967
selectjundev <selectjun.dev@gmail.com>
2023-09-24 11:08:20 +0900

    [ build ] spring boot/cloud 및 java 버전 업

--- src/main/java/com/attendancecheck/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/attendancecheck/gatewayserver/GatewayServerApplication.java
@@ -2,10 +2,10 @@ package com.attendancecheck.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
